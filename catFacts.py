cadfe0e52fc7230e03ee59c4da3d466017f0a0f6
Jacob Ellis <jellis5@memphis.edu>
2015-08-28 21:55:57 -0500

    Refactored

--- catFacts.py
+++ catFacts.py
@@ -1,22 +1,40 @@
-#!/usr/bin/env python
-import urllib.request 
-from texter import *
-import time
+#!/usr/bin/env python3
+
+from urllib.request import urlopen
+from smtplib import SMTP
+from email.mime.text import MIMEText
+from time import sleep
+
+email_username = 'daheadofstate@gmail.com'
+email_password = 'testing777'
 
 def getRandomWord():
-	req = urllib.request.Request("https://www.randomlists.com/random-words")
-	resp = urllib.request.urlopen(req).read()
-	for x in range(0,len(resp)-51,50):
-		line = str(resp[x:x+50])
-		if "<span class='crux'" in line:
-			return line[line.find('crux')+5:line.find('</span>')]
+	with urlopen("http://www.randomlists.com/random-words") as resp:
+		for line in resp:
+			line = str(line)
+			if "crux" in line:
+				# + 7 to advance past 'crux' and angle brackets
+				wordStartIndex = line.find('crux') + 7
+				wordEndIndex = line[wordStartIndex:].find('</span>')
+				if wordEndIndex == -1:
+					continue
+				return line[wordStartIndex:wordStartIndex+wordEndIndex]
+				
+def getEmailFromPN():
+	pass
+
+def sendText(smtp_object, message, phone_number):
+	message = MIMEText(message)
+	smtp_object.login(email_username, email_password)
+	smtp_object.sendmail(email_username, phone_number+'@txt.att.net', message.as_string())
 
 def catFacts(pn):
-	cf = open("catfacts.txt", "r")
-	lines = cf.readlines()
-	for line in lines:
-		sendText("Thanks for signing up for CatFacts! You have elected to receive a fact about cats via text once every minute! Reply '" + getRandomWord()+ "' to stop further catfacts!",pn)
-		sendText(line,pn)
-		time.sleep(60)
+	s = SMTP('smtp.gmail.com:587')
+	s.starttls()
+	with open("catfacts.txt", "r") as cf:
+		for line in cf:
+			sendText(s, "Thanks for signing up for CatFacts! You have elected to receive a fact about cats via text once every minute! Reply '" + getRandomWord() + "' to stop further catfacts!", pn)
+			sendText(s, line, pn)
+			sleep(60)
 
 catFacts(input("Phone Number: "))
